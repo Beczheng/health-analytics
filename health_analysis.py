f204c78bfb7736493599e3eb3521e333dfa357b6
beczheng <beckie.zheng101@gmail.com>
2023-08-18 22:39:56 +0000

    more changes

--- health_analysis.py
+++ health_analysis.py
@@ -52,36 +52,36 @@ PatientC = {
 
 def MCV_result(Name, MCV):
     if MCV < 80:
-        result = Name + ": RBCs are smaller than normal"
+        result = "RBCs are smaller than normal for " + Name, MCV
     elif MCV >= 80 and MCV <= 100:
-        result = Name + ": RBCs are normal in size"
+        result = "RBCs are smaller than normal for " + Name, MCV
     else:
-        result = Name + ": RBCs are bigger than normal" 
-    return result
+        result = "RBCs are smaller than normal for " + Name, MCV
+    return result 
 
-Patient_result = MCV_result("Harry Potter", 86)
-print(Patient_result)
+Patient_MCV_result = MCV_result("Harry Potter", 86)
+print(Patient_MCV_result)
 
-Patient_result = MCV_result("Ron Weasley", 70)
-print(Patient_result)
+Patient_MCV_result = MCV_result("Ron Weasley", 70)
+print(Patient_MCV_result)
 
-Patient_result = MCV_result("Hermione Granger", 103)
-print(Patient_result)
+Patient_MCV_result = MCV_result("Hermione Granger", 103)
+print(Patient_MCV_result)
 
 # This is a list of the urine color results
 
 def Urine_color_result(Name, Color):
     if Color == "Yellow":
-        result = Name + ": the urine color is normal"
+        result = "The urine color is normal for " + Name, Color
     else:
-        result = Name + ": the urine color is not normal"
+        result = "The urine color is not normal for " + Name, Color
     return result
 
-Patient_result = Urine_color_result("Harry Potter", "Yellow")
-print(Patient_result)
+Patient_urine_color_result = Urine_color_result("Harry Potter", "Yellow")
+print(Patient_urine_color_result)
 
-Patient_result = Urine_color_result("Ron Weasley", "Black")
-print(Patient_result)
+Patient_urine_color_result = Urine_color_result("Ron Weasley", "Black")
+print(Patient_urine_color_result)
 
-Patient_result = Urine_color_result("Hermione Granger", "Yellow")
-print(Patient_result)
+Patient_urine_color_result = Urine_color_result("Hermione Granger", "Yellow")
+print(Patient_urine_color_result)
